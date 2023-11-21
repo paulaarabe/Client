b1b26f5382d204346ff6b72ab1eeeec55c023b52
Paula Arabe <arabepaula1011@gmail.com>
2023-11-21 15:35:09 +0800

    update text

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -6,11 +6,11 @@
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('clients.index')}}">
                 <i class="bi bi-box-seam"></i>
-                <span>Product</span>
+                <span>Client</span>
             </a>
 
 
         </li>
     </ul>
 
-</aside><!-- End Sidebar-->
+</aside>
